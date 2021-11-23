b4d325332af773c9a1344a7b8c6504754ead921a
andreaspribitzer <andreas.pribitzer@outlook.at>
2021-11-23 09:32:55 +0100

    Adapt to spring-boot-example

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('buildJob') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/Apri34/spring-boot-example.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
